1b8accad76cdeae9434003e018806fc01c4a4844
unknown <paritoshrawat21@gmail.com>
2024-03-28 13:12:30 +0530

    change in productApi and producrslice

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchCount } from "./counterAPI";
+import { fetchCount } from "./CartAPI";
 
 const initialState = {
   value: 0,
@@ -10,7 +10,7 @@ export const incrementAsync = createAsyncThunk(
   "counter/fetchCount",
   async (amount) => {
     const response = await fetchCount(amount);
-
+    // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
@@ -18,13 +18,11 @@ export const incrementAsync = createAsyncThunk(
 export const counterSlice = createSlice({
   name: "counter",
   initialState,
-
   reducers: {
     increment: (state) => {
       state.value += 1;
     },
   },
-
   extraReducers: (builder) => {
     builder
       .addCase(incrementAsync.pending, (state) => {
